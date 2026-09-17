7fd111c8f2121068a2ead1464ac1ac8f36df3133
unknown <vishwaae>
2026-09-17 16:42:48 +0530

    Rewrite vault module: real HashiCorp yum repo, config.hcl template, UI enabled, fully Hiera-driven

--- modules/vault/manifests/init.pp
+++ modules/vault/manifests/init.pp
@@ -3,21 +3,30 @@ class vault (
   Boolean $manage_user      = lookup('vault::manage_user'),
   String  $group            = lookup('vault::group'),
   Boolean $manage_group     = lookup('vault::manage_group'),
-  String  $bin_dir          = lookup('vault::bin_dir'),
   String  $config_dir       = lookup('vault::config_dir'),
   Boolean $purge_config_dir = lookup('vault::purge_config_dir'),
   String  $service_name     = lookup('vault::service_name'),
   Boolean $service_enable   = lookup('vault::service_enable'),
-  String  $version          = lookup('vault::version'),
   String  $package_ensure   = lookup('vault::package_ensure'),
-  String  $package_provider = lookup('vault::package_provider'),
-  String  $package_source   = lookup('vault::package_source'),
+  String  $repo_baseurl     = lookup('vault::repo_baseurl'),
+  String  $repo_gpgkey      = lookup('vault::repo_gpgkey'),
+  String  $listen_address   = lookup('vault::listen_address'),
+  Integer $listen_port      = lookup('vault::listen_port'),
+  Boolean $ui_enabled       = lookup('vault::ui_enabled'),
+  Boolean $tls_disable      = lookup('vault::tls_disable'),
 ) {
 
+  yumrepo { 'hashicorp':
+    baseurl  => $repo_baseurl,
+    descr    => 'HashiCorp Stable - $basearch',
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => $repo_gpgkey,
+  }
+
   package { 'vault':
-    ensure   => $package_ensure,
-    provider => $package_provider,
-    source   => $package_source,
+    ensure  => $package_ensure,
+    require => Yumrepo['hashicorp'],
   }
 
   user { $user:
@@ -35,9 +44,16 @@ class vault (
     recurse => true,
   }
 
+  file { "${config_dir}/config.hcl":
+    ensure  => file,
+    content => template('vault/config.hcl.erb'),
+    require => File[$config_dir],
+    notify  => Service[$service_name],
+  }
+
   service { $service_name:
     ensure  => running,
     enable  => $service_enable,
-    require => Package['vault'],
+    require => [Package['vault'], File["${config_dir}/config.hcl"]],
   }
 }
